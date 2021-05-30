20dc5dc699e2e94a1be30c7e29ab4f58efee0b47
YazanAlkarabshe <>
2021-05-30 12:20:09 +0000

    add changes

---
+++ app.js
@@ -0,0 +1,10 @@
+var userName =prompt('what is your name')
+alert('welcome to our website '+userName)
+
+var userRating=prompt('please rate our website from 1 to 5?')
+for (let i=0 ; i<userName ; i++)
+{
+  document.write('<img src="https://affaso.com/wp-content/uploads/2020/06/5-point-stars-png-star-icon-flat-11562958768wpf63hu4tq.png"/>')
+
+}
+
